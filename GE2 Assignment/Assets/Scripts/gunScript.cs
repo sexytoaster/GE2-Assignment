d8770cb60b9c0face65bc2241089e355c7398ee6
sexytoaster <paddyoconnor1@hotmail.com>
2020-05-17 23:47:32 +0100

    working on lasers

---
+++ GE2 Assignment/Assets/Scripts/gunScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunScript : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public float speed = 2000;
+    public float shotsPerMin = 10;
+    public float lastShot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastShot = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - lastShot >= 60 / shotsPerMin)
+        {
+            GameObject instantiatedBullet = Instantiate(bullet, transform.position, transform.rotation);
+
+            instantiatedBullet.GetComponent<Rigidbody>().velocity += new Vector3(-speed, 0, 0);
+            lastShot = Time.time;
+        }
+        
+    }
+}
